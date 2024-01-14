81502d53a6f262ed60811c7b3a6aa60f6ed7e97b
Guilherme Alvarez <guilherme.zalv@gmail.com>
2024-01-14 19:06:44 -0300

    Teste

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente App', () => {
+  test('Testando os links da Navegação', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByText('Home');
+    const linkAbout = screen.getByText('About');
+    const linkFavorite = screen.getByText('Favorite Pokémon');
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
+
+// Testando primeiros testes.
