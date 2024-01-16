ebb392370e8b5a59bc6f83ac275ff53bdaf5371d
Guilherme Alvarez <guilherme.zalv@gmail.com>
2024-01-16 02:57:28 -0300

    Req 7 feito e lint corrigido

--- src/tests/PokemonDetails.test.tsx
+++ src/tests/PokemonDetails.test.tsx
@@ -1,10 +1,14 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
+const dragLocal = 'Dragonair location';
+const dragPage = '/pokemon/148';
+
 describe('Testando o componente PokemonDetails', () => {
   test('Testando as informações detalhadas', () => {
-    renderWithRouter(<App />, { route: '/pokemon/148' });
+    renderWithRouter(<App />, { route: dragPage });
     const pokDet = screen.getByRole('heading', { name: 'Dragonair Details' });
     expect(pokDet).toBeInTheDocument();
 
@@ -19,12 +23,12 @@ describe('Testando o componente PokemonDetails', () => {
   });
 
   test('Seção mapas', () => {
-    renderWithRouter(<App />, { route: '/pokemon/148' });
+    renderWithRouter(<App />, { route: dragPage });
     const pokLocations = screen.getByRole('heading', { name: 'Game Locations of Dragonair' });
     expect(pokLocations).toBeInTheDocument();
 
     // Pegando as localizações:
-    const imgsLocaisDragonair = screen.getAllByAltText('Dragonair location');
+    const imgsLocaisDragonair = screen.getAllByAltText(dragLocal);
     expect(imgsLocaisDragonair).toHaveLength(2);
 
     imgsLocaisDragonair.forEach((imagem) => {
@@ -38,9 +42,28 @@ describe('Testando o componente PokemonDetails', () => {
     expect(srcDoSegundoElemento).toBe('https://archives.bulbagarden.net/media/upload/1/1e/Johto_Dragons_Den_Map.png');
 
     const altDoPrimeiroElemento = imgsLocaisDragonair[0].getAttribute('alt');
-    expect(altDoPrimeiroElemento).toBe('Dragonair location');
+    expect(altDoPrimeiroElemento).toBe(dragLocal);
 
     const altDoSegundoElemento = imgsLocaisDragonair[1].getAttribute('alt');
-    expect(altDoSegundoElemento).toBe('Dragonair location');
+    expect(altDoSegundoElemento).toBe(dragLocal);
+
+    const textoMapa1 = screen.getByText('Johto Route 45');
+    expect(textoMapa1).toBeInTheDocument();
+    const textoMapa2 = screen.getByText('Johto Dragon\'s Den');
+    expect(textoMapa2).toBeInTheDocument();
+  });
+
+  test('Favoritar na pag detalhes', async () => {
+    renderWithRouter(<App />, { route: dragPage });
+    const checkbox = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    expect(checkbox).toBeInTheDocument();
+
+    await userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    await userEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+
+    const checkboxLabel = screen.getByLabelText('Pokémon favoritado?');
+    expect(checkboxLabel).toBeInTheDocument();
   });
 });
