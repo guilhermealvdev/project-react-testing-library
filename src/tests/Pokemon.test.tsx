6f2ad62507b6d8de0add314591403dbe741d0472
Guilherme Alvarez <guilherme.zalv@gmail.com>
2024-01-16 00:59:44 -0300

    Progredindo Req 6

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,33 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { Pokemon } from '../components';
+
+describe('Testando o componente Pokemon', () => {
+  test('É renderizado o card com as informações do pokemon', async () => {
+    renderWithRouter(<App />);
+    const nomePokemon = screen.getByTestId('pokemon-name');
+    expect(nomePokemon).toBeInTheDocument();
+
+    const tipoPokemon = screen.getByTestId('pokemon-type');
+    expect(tipoPokemon).toBeInTheDocument();
+
+    const pesoPokemon = screen.getByTestId('pokemon-weight');
+    expect(pesoPokemon).toBeInTheDocument();
+
+    const imgPokemon = screen.getByAltText('Pikachu sprite');
+    expect(imgPokemon).toBeInTheDocument();
+    expect(imgPokemon).toHaveAttribute('alt', 'Pikachu sprite');
+    expect(imgPokemon).toHaveAttribute('src', 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  test('Testando link', () => {
+    renderWithRouter(<App />);
+    const linkElement = screen.getByRole('link', { name: 'More details' });
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', '/pokemon/25');
+  });
+
+  // Em progresso
+});
